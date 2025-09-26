a22b734d72754ab2b1642b36a6a7860e4bde9eef
Albert <AlbertNkhata@hotmail.com>
2025-09-26 17:11:12 +0000

    Fix footer placement and background in layout

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="flex flex-col min-h-screen bg-gray-100 antialiased">
         <Header />
         <main>{children}</main>
         <Footer />
