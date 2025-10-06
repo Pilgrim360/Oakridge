2f209839efd28a84989953c36479d759fb833720
Albert <AlbertNkhata@hotmail.com>
2025-10-06 11:37:23 +0000

    Move SectionTitle after Overview on homepage

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,11 +1,17 @@
 import Hero from "@/components/Hero";
 import Overview from "@/components/Overview";
 
+import SectionTitle from "@/components/SectionTitle";
+
 export default function Home() {
   return (
     <>
       <Hero />
       <Overview />
+      {/* Services Section Title */}
+      <SectionTitle centered subtitle="Discover what we offer">
+        Our Services
+      </SectionTitle>
     </>
   );
 }
